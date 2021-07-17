383a6d537037f38b25c6def462d208b346979d38
lat9nq <22451773+lat9nq@users.noreply.github.com>
2021-07-17 17:25:19 -0400

    travis: Use yuzu user for building

--- .travis/exec.sh
+++ .travis/exec.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -ex
 
-docker run -v $(pwd):/liftinstall -t yuzuemu/build-environments:linux-liftinstall /bin/bash /liftinstall/.travis/build.sh $(id -u) $(id -g)
+# the UID for the container yuzu user is 1027
+sudo chown -R 1027 ./
+docker run -v $(pwd):/liftinstall -t yuzuemu/build-environments:linux-liftinstall /bin/bash /liftinstall/.travis/build.sh sudo chown -R $(id -u):$(id -g) ./
